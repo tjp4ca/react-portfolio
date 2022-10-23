198b6de717294edf83bb78a3a2a28a0cb5bfd1f3
Michael Park <tjp4ca@gmail.com>
2022-10-22 23:36:02 -0400

    using router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
 import React from "react";
-import Header from './components/Header';
-import Project from './components/Project';
-import Footer from './components/Footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// import Header from './components/Header';
+// import Project from './components/Project';
+// import Footer from './components/Footer';
 
 import './App.css';
+// import Home from "./pages/Home"
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
+import Resume from "./pages/Resume";
 
 function App() {
 
   return (
     <div>
-      <Header />
+      <Router>
+        <div>
+          <Routes>
+            {/* <Route 
+                path="/" 
+                element={<Home />} 
+            /> */}
+
+            <Route 
+                path="/aboutme" 
+                element={<About />} 
+            />
+
+            <Route 
+                path="/portfolio" 
+                element={<Portfolio />} 
+            />
+
+            <Route 
+                path="/contact" 
+                element={<Contact />} 
+            />
+
+            <Route 
+                path="/resume" 
+                element={<Resume />} 
+            />
+          </Routes>
+        </div>
+      </Router>
+      {/* <Header />
 
       <Project />
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,36 +1,30 @@
-import React, { useState } from 'react';
-import Navigation from '../Navigation';
-import About from '../pages/About';
-import Contact from '../pages/Contact';
-import Portfolio from '../pages/Portfolio';
-import Resume from '../pages/Resume';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// import React, { useState } from 'react';
+// import Navigation from '../Navigation';
+// import About from '../pages/About';
+// import Contact from '../pages/Contact';
+// import Portfolio from '../pages/Portfolio';
+// import Resume from '../pages/Resume';
 
 function Header() {
-    const [currentPage, setCurrentPage] = useState('Home');
-
-    const renderPage = () => {
-        // if (currentPage === 'Home') {
-        //     return <Home />;
-        // }
-        if (currentPage === 'About') {
-            return <About />;
-        }
-        if (currentPage === 'Contact') {
-            return <Contact />;
-        }
-        if (currentPage === 'Portfolio') {
-            return <Portfolio />;
-        }
-        return <Resume />;
-      };
-    
-    const handlePageChange = (page) => setCurrentPage(page);
 
     return (
-        <div>
-        <Navigation currentPage={currentPage} handlePageChange={handlePageChange} />
-        {renderPage()}
-        </div>
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <Link className="navbar-brand" to="/">Title</Link>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div className="navbar-nav">
+      <Link className="nav-item nav-link active" to="/aboutme">About Me <span className="sr-only">(current)</span></Link>
+      <Link className="nav-item nav-link" to="/portfolio">Portfolio</Link>
+      <Link className="nav-item nav-link" to="/contact">Contact</Link>
+      <Link className="nav-item nav-link" to="/resume">Resume</Link>
+    </div>
+  </div>
+</nav>
     )
 }
 
